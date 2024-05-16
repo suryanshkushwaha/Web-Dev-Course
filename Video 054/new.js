eb80a18a6b1f3b56e991dbd9300135b0303dd4a5
Suryansh Kushwaha <112935210+suryanshkushwaha@users.noreply.github.com>
2024-05-16 12:40:45 +0800

    Video 54 uploaded
    JavaScript Intro and Node.js installed

---
+++ Video 054/new.js
@@ -0,0 +1,3 @@
+console.log("Downloaded Node js...")
+console.log("Testing...")
+console.log("Code is running like a wow...")
---
+++ Video 054/script.js
@@ -0,0 +1,21 @@
+alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is running like a wow...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure want to leave this page and blast your computer?")
+
+console.log("Your number is " + a)
+
+if (isTrue) {
+    console.log("Computer is blasting")
+} else {
+    console.log("No it's not blasting")
+}
+
+document.title = "Hey I am good!"
+// document.body.style.backgroundColor = "red"
+
+// JS adds inline CSS
