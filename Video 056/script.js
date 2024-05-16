42426d28bff933dc7f82767e8223b02a63268448
Suryansh Kushwaha <112935210+suryanshkushwaha@users.noreply.github.com>
2024-05-16 19:31:39 +0800

    Video 56 uploaded
    JS Conditionals

---
+++ Video 056/script.js
@@ -0,0 +1,21 @@
+// Java Conditionals
+
+console.log("Hello I am conditional tutorial")
+
+let age = 45
+
+if (age > 18) {
+    console.log("you can drive")
+} else {
+    console.log("you cant drive!")
+}
+
+if (age = 18) { //it changes the value and evaluates to true
+    console.log("you can drive")
+} else {
+    console.log("you cant drive!")
+}
+
+console.log ( "3" == 3) // checks only the value
+console.log ( "3" === 3) // checks value and the TYPE
+
