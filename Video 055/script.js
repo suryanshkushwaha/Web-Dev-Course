e28709ddd9829c9bc2abf60e7f948ec4b3cb3140
Suryansh Kushwaha <112935210+suryanshkushwaha@users.noreply.github.com>
2024-05-16 18:30:46 +0800

    Video 55 uploaded
    Variables, Data types and Objects

---
+++ Video 055/script.js
@@ -0,0 +1,29 @@
+console.log("Hey this is tutorial 55")
+
+var a=5;
+var b=6;
+var c="Harry"
+var _ab = "you"
+// var 44h = "invalid"
+
+console.log(a+b+c)
+console.log(typeof a, typeof b, typeof c)
+
+const a1 = 6;
+// a1 = a1 + 1; /* TypeError: Assignment to constant variable. */
+a= a +1;
+
+/* var vs let
+var always has GLOBAL scope
+let has global and block scope as well */
+
+// type of null is object!! 
+let check = null
+console.log(typeof check)
+
+let worker = {
+    "name": "harry",
+ 'job code': 4500
+}
+
+console.log(worker)
