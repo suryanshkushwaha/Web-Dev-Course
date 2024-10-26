17857cea05a7cb0e47328dcd1ed4dea9efe52249
Suryansh Kushwaha <112935210+suryanshkushwaha@users.noreply.github.com>
2024-10-26 22:50:53 +0800

    Video 105
    Introduction to React

---
+++ Video 105/with-react/src/App.js
@@ -0,0 +1,19 @@
+import {useState} from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className='value'>{value}</div>
+      <button onClick={()=> {setValue(value + 1)}}>Click Me</button>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Video 105/with-react/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      I am a footer.
+    </div>
+  )
+}
+
+export default Footer
