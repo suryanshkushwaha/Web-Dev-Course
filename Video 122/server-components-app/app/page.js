0e70de11acb4cb2110e323adaae7c703dfc8fdd6
Suryansh Kushwaha <112935210+suryanshkushwaha@users.noreply.github.com>
2024-10-28 01:48:04 +0800

    Video 122
    Server Components in Next.js

---
+++ Video 122/server-components-app/app/page.js
@@ -0,0 +1,19 @@
+// "use client"
+// import { useState, useEffect } from "react";
+import fs from "fs/promises"
+import Navbar from "@/components/Navbar"
+
+export default function Home() {
+  // const [count, setCount] = useState(0)
+  console.log("Hey I am Suryansh")
+  let a = fs.readFile(".gitignore")
+  a.then(e=>{console.log(e.toString())})
+  return (
+   <div>
+    <Navbar/>
+    I am a component 
+    {/* {count}
+    <button onClick={()=> setCount(count + 1)}>Click me</button> */}
+   </div>
+  );
+}
